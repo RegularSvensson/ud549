51f145c91a624dcb2650952fabcb383e4aca412f
RegularSvensson <elias.svensson.1992@gmail.com>
2015-04-09 16:23:36 +0200

    Fix spelling error

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -2,7 +2,5 @@ function AddressBook() {
 	this.contacts = [];
 }
 
-AdressBook.prototype.addContact = function(contact) {
 AddressBook.prototype.addContact = function(contact) {
 	this.contacts.push(contact);
-}
